49b4db7966f4220e0ed30b0ff7630bf77570e2a7
Sarun Dahal <sarundahal11@gmail.com>
2020-11-24 19:02:26 +0545

    adds serializer for Post model

--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from .models import Post
+from .serializers import PostSerializer
+
 # third party imports
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 
 # Create your views here.
 
@@ -11,19 +15,19 @@ from rest_framework.response import Response
 class TestView(APIView):
     
     def get(self, request, *args, **kwargs):
-        data = {
-            'book': 30,
-            'Pen': 10
-        } 
-        return Response(data)
-    
-    def post(self, request, *args, **kwargs):
-        data = {
-            'book': 40,
-            'Pen': 10
-        } 
-        return Response(data)
         
+        qs = Post.objects.all()
+        # post = qs.last()
+        serializer = PostSerializer(qs, many=True)
+        # serializer = PostSerializer(post)
+        return Response(serializer.data)
+          
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)       
 
 # def test_view(request):
 #     data = {
