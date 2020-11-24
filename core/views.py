47dd4bf3bee36432195b4a115399e9bad66a2692
Sarun Dahal <sarundahal11@gmail.com>
2020-11-24 17:03:30 +0545

    basic API

--- core/views.py
+++ core/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+# third party imports
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
-def test_view(request):
-    data = {
-        'book': 30,
-        'Pen': 10
-    }
-   
-    return JsonResponse(data)
+
+
+class TestView(APIView):
+    
+    def get(self, request, *args, **kwargs):
+        data = {
+            'book': 30,
+            'Pen': 10
+        } 
+        return Response(data)
+    
+    def post(self, request, *args, **kwargs):
+        data = {
+            'book': 40,
+            'Pen': 10
+        } 
+        return Response(data)
+        
+
+# def test_view(request):
+#     data = {
+#         'book': 30,
+#         'Pen': 10
+#     }
+#     return JsonResponse(data)
